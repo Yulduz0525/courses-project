7625e6a8236c05c2064224c76ec15268fae8b318
@yunusovayulduz85 <yunusovayulduz85@gmail.com>
2024-09-14 18:26:03 +0500

    output.css deleted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
-import plugin from 'tailwindcss';
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [
-    // plugin(function({addBase}){
-    //   addBase({
-    //     //Default colors
-    //     ':root':{
-    //       '--main-color':toRGB('#121421')
-    //     }
-    //   })
-    // })
   ],
 }
 
